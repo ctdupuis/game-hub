cba869f92beebd151a39fb293907b0d4c158eda1
ctdupuis <54044737+ctdupuis@users.noreply.github.com>
2020-01-06 11:40:18 -0600

    added authentication to user home

--- game-hub/app/controllers/users_controller.rb
+++ game-hub/app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
 
     get "/home" do
-        erb :'users/home'
+      @current_user = User.find_by_id(session[:user_id])
+      if @current_user
+          erb :'users/home'
+      else
+          erb :'users/error'
+      end
     end
     
 end
